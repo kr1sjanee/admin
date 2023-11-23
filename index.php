0fe5324f5205ddfec179d353d741579db6656004
Kris Jane Cabilan <cabilankrisjane@gmail.com>
2023-11-23 21:03:22 +0800

    rename the admin

--- index.php
+++ index.php
@@ -21,7 +21,6 @@
 </head>
 
 <body>
-    <!-- .wrapper. -->
     <div class="wrapper">
       <!-- Main Sidebar Container -->
       <aside class="main-sidebar sidebar-dark-primary elevation-4">
@@ -37,10 +36,10 @@
                 <!-- Sidebar user panel (optional) -->
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="image">
-                        <img src="img/krisjane.jpg" class="img-circle elevation-2" alt="User Image">
+                    <img src="img/krisjane.jpg" class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
-                        <a href="#" class="d-block">Alexander Pierce</a>
+                        <a href="#" class="d-block">Kris Jane Cabilan</a>
                     </div>
                 </div>
 
